829010e4db795fac06825d45dc3bcc4bfaf19878
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2024-05-13 14:58:59 -0300

    Create poo_python.py

---
+++ poo_python.py
@@ -0,0 +1,16 @@
+class Pessoa:
+  def __init__(self, nome, endereco):
+    self.set_nome(nome)
+    self.set_endereco(endereco)
+
+    def set_nome(self, nome):
+      self.nome=nome
+
+    def set_endereco(self, endereco):
+      self.endereco=endereco
+    
+    def get_nome(self):
+      return self.nome
+
+    def get_endereco(self):
+      return self.endereco  

c3f978deba9e039b9eb68aafb137dac7a6c98466
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2024-05-13 15:06:02 -0300

    Update poo_python.py

--- poo_python.py
+++ poo_python.py
@@ -3,14 +3,21 @@ class Pessoa:
     self.set_nome(nome)
     self.set_endereco(endereco)
 
-    def set_nome(self, nome):
-      self.nome=nome
+  def set_nome(self, nome):
+    self.nome = nome
 
-    def set_endereco(self, endereco):
-      self.endereco=endereco
+  def set_endereco(self, endereco):
+    self.endereco = endereco
     
-    def get_nome(self):
-      return self.nome
+  def get_nome(self):
+    return self.nome
 
-    def get_endereco(self):
-      return self.endereco  
+  def get_endereco(self):
+    return self.endereco  
+
+pessoa1 = Pessoa("Maria", "Rua 01234")
+pessoa2 = Pessoa("João", "Rua 56789")
+
+print(f'Nome: {pessoa1.get_nome()}, Endereço: {pessoa1.get_endereco()}')
+
+print(f'Nome: {pessoa2.get_nome()}, Endereço: {pessoa2.get_endereco()}')
